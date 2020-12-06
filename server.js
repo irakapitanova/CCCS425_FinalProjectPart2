b40d52340c674522d3385a2e8d17fb113f5d013b
irakapitanova <ira.kapitanova@guavus.com>
2020-12-05 19:16:36 -0500

    Change the port

--- server.js
+++ server.js
@@ -730,6 +730,4 @@ app.get("/selling", (req, res) => {
 })
 
 // listen for requests :)
-const listener = app.listen(process.env.PORT, () => {
-  console.log("Your app is listening on port " + listener.address().port);
-});
+app.listen(process.env.PORT || 3000)
